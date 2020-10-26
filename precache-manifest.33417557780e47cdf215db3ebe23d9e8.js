b776037b22779265442fcbdc75b83d76ae9b40b1
martakupka <marta-k2008@ukr.net>
2020-10-26 13:51:54 +0200

    Updates

---
+++ precache-manifest.33417557780e47cdf215db3ebe23d9e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9639056fe5ac56a933c6560f0ba0d3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4c7dd72877c169f9278",
+    "url": "./static/css/main.9e30de10.chunk.css"
+  },
+  {
+    "revision": "2b35cc6fc0ebfd8c56b1",
+    "url": "./static/js/2.128b5140.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.128b5140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c7dd72877c169f9278",
+    "url": "./static/js/main.8679ad7d.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
